29518e42a59e168fe152d6f5215d6754dcfad642
Roby1505 <patainearobertpp@gmail.com>
2020-05-27 19:03:16 +0300

    added counter

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,11 @@ function watchCode(){
     watch("./scss/style.scss" , css);
 }
 
+function getJs(){
+    return src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(dest('js/'));
+}
+
 exports.css = css;
 exports.watchCode = watchCode;
+exports.getJs = getJs;
